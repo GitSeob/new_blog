78ce0f524ff670ea5b1a452c6fa0708e442bb0f0
GitSeob <anhs0220@gmail.com>
2021-03-09 13:24:43 +0900

    서버 켜지는 상태일 때 처리

--- front/pages/_error.tsx
+++ front/pages/_error.tsx
@@ -5,7 +5,20 @@ interface ErrorProps {
 	message?: string | null;
 }
 
-function Error({ statusCode, message = null }: ErrorProps) {
+const PleaseRefresh = () => {
+	return (
+		<ErrorContainer>
+			<img src="/refresh.png" />
+			<p>반갑습니다! 정말 간만의 방문객이세요!</p>
+			<p>그래서 서버가 천천히 켜지고 있습니다</p>
+			<p>10초 후에 새로고침해주시면 정상적으로 보일거에요!</p>
+		</ErrorContainer>
+	);
+};
+
+const Error = ({ statusCode, message = null }: ErrorProps) => {
+	if (statusCode === 408) return <PleaseRefresh />;
+
 	return (
 		<ErrorContainer>
 			<img src={statusCode === 404 ? '/404.svg' : '/error.png'} />
@@ -16,7 +29,7 @@ function Error({ statusCode, message = null }: ErrorProps) {
 			)}
 		</ErrorContainer>
 	);
-}
+};
 
 interface GIPProps {
 	res: any;
